ce297063dd1fe165d8f1dfaa0344b824fd2d2694
takumi616 <ohohyeah321@icloud.com>
2025-05-11 03:04:46 +0900

    case statement sample2

--- chap-1/case/test_data.sql
+++ chap-1/case/test_data.sql
@@ -1,13 +1,14 @@
-insert into engineers (sex, age, country, job_role) values
-('1', 30, 'USA', 'backend'),
-('1', 35, 'JAPAN', 'machine learning'),
-('2', 28, 'UK', 'backend'),
-('2', 29, 'JAPAN', 'backend'),
-('1', 38, 'UK', 'frontend'),
-('2', 28, 'USA', 'machine learning'),
-('2', 33, 'USA', 'frontend'),
-('2', 24, 'JAPAN', 'machine learning'),
-('1', 41, 'UK', 'backend'),
-('1', 45, 'JAPAN', 'backend'),
-('2', 28, 'UK', 'frontend'),
-('1', 37, 'USA', 'machine learning');
+insert into engineers (sex, age, country, position, salary, monitor) values
+('1', 30, 'USA', 'backend', 100000, 3),
+('1', 35, 'JAPAN', 'machine learning', 100050, 2),
+('2', 28, 'UK', 'backend', 100100, 3),
+('2', 29, 'JAPAN', 'backend', 100000, 2),
+('1', 38, 'UK', 'frontend', 100010, 2),
+('2', 28, 'USA', 'machine learning', 100080, 3),
+('2', 33, 'USA', 'frontend', 100040, 3),
+('2', 24, 'JAPAN', 'machine learning', 100100, 2),
+('1', 41, 'UK', 'backend', 100020, 2),
+('1', 45, 'JAPAN', 'backend', 100020, 3),
+('2', 28, 'UK', 'frontend', 100030, 2),
+('1', 37, 'USA', 'machine learning', 100200, 3),
+('1', 28, 'GERMANY', 'backend', 100130, 2);
